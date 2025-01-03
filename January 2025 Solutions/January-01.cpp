972d5ab5d85becaf2350a0cd02c558856497a6df
Sana9909 <snehalahiri09@gmail.com>
2025-01-03 23:11:12 +0530

    Day 1 Streak

---
+++ January 2025 Solutions/January-01.cpp
@@ -0,0 +1,30 @@
+// 1422. Maximum Score After Splitting a String
+// https://leetcode.com/problems/maximum-score-after-splitting-a-string/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int ones = 0;
+        int zeros = 0;
+        int best = INT_MIN;
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+            
+            best = max(best, zeros - ones);
+        }
+        
+        if (s[s.size() - 1] == '1') {
+            ones++;
+        }
+        
+        return best + ones;
+    }
+};
